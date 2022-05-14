b82f40c7e727536eb06026c5465285555c391092
Avid3 <56166127+Avid3@users.noreply.github.com>
2022-05-14 13:46:28 -0400

    Add files via upload

--- MultipleLinearCode_DataVisualization.py
+++ MultipleLinearCode_DataVisualization.py
@@ -16,29 +16,33 @@ data=pd.read_csv('Nutrition__Physical_Activity__and_Obesity_-_Behavioral_Risk_Fa
 
 # fig, ax = plt.subplots(figsize=(10,6))
 # sns.heatmap(data.corr())
-# "This now applies to Alabama only. Will be extended to the rest of the US too"
+"This now applies to Alabama only. Will be extended to the rest of the US too"
 
-# data=data[data['LocationAbbr'].str.contains("US")==True]
+data=data[data['LocationAbbr'].str.contains("PA")==True]
 
-# data=data[data['Total'].str.contains("Total")==True]
+# data=data[data['Education'].str.contains("nan")==False]
 
 
-# data=data[data['Topic'].str.contains("Obesity / Weight Status")==True]
-# data1=data['YearStart']
-# data2=data['Data_Value']
-
-# data3={'col1':data1,'col2':data2}
-
-# data4=pd.DataFrame(data=data3)
-# m, b = np.polyfit(data4['col1'], data4['col2'], 1)
-
-# plt.scatter(data4['col1'],data4['col2']),plt.title("Obesity Rates in US Yearwise"),plt.xlabel('Year'),plt.ylabel('Obesity rate in %')
-
-# plt.plot(data4['col1'],m*data4['col1']+b)
-
+data=data[data['Topic'].str.contains("Obesity / Weight Status")==True]
+data=data[data['Question'].str.contains("Percent of adults aged 18 years and older who have obesity")==True]
 data=data[data['Stratification1'].str.contains("Total")==True]
-data=data[data['LocationDesc'].str.contains("National")==True]
-data5=data[data['Topic'].str.contains("Obesity / Weight Status")==True]
-data6=data[data['Topic'].str.contains("Physical Activity - Behavior")==True]
+
+
+data1=data['YearStart']
+data2=data['Data_Value']
+
+data3={'col1':data1,'col2':data2}
+
+data4=pd.DataFrame(data=data3)
+m, b = np.polyfit(data4['col1'], data4['col2'], 1)
+
+plt.scatter(data4['col1'],data4['col2']),plt.title("Obesity Rates in AL Yearwise"),plt.xlabel('Year'),plt.ylabel('Obesity rate in %')
+
+plt.plot(data4['col1'],m*data4['col1']+b)
+
+
+# data=data[data['LocationDesc'].str.contains("National")==True]
+# data5=data[data['Topic'].str.contains("Obesity / Weight Status")==True]
+# data6=data[data['Topic'].str.contains("Physical Activity - Behavior")==True]
 
 # data7=data6[data6['Question'].str.contains("Percent of adults who achieve at least 150 minutes a week of moderate-intensity aerobic physical activity or 75 minutes a week of vigorous-intensity aerobic activity (or an equivalent combination)")==True]
